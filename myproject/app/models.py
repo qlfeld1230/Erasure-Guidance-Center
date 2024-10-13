6f7ddff505553fd2ffa91c97858babef20512bd8
Dong-Han <donghk77@naver.com>
2024-10-13 20:24:55 +0900

    feat : update etc. files

--- myproject/app/models.py
+++ myproject/app/models.py
@@ -1,6 +1,2 @@
 from django.db import models
 
-class UserModel(models.Model):
-    id = models.CharField(max_length=20, primary_key=True)
-    password = models.CharField(max_length=20, null=False)
-
