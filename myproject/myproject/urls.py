53bf0f2e111f3cef1a03cb5d1ab82a22663cf23d
Dong-Han <donghk77@naver.com>
2024-10-13 20:25:41 +0900

    feat : update urls

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import path
-from app.views.page_view import *
-from app.views.crawler_view import *
+from app.views.page_view import MainView, EnrollView
+from app.views.crawler_view import crawler_view
+from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
-    path('main/', main),
-    path('login/', login),
-    path('enroll/', enroll),
-    
+    path('', MainView.as_view()),
+    path('admin/', admin.site.urls), 
+    path('login/', LoginView.as_view(template_name='login.html', success_url='/')),
+    path('logout/', LogoutView.as_view(next_page='/login/')), 
+    path('enroll/', EnrollView.as_view()),
     path('crawler/', crawler_view),
-
 ]
