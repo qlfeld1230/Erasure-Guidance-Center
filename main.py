563539474c05b4d4d9dbaab5586fdfe1f765de5f
Eunjieater <72612755+Eunjieater@users.noreply.github.com>
2024-10-25 13:47:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 query = input('뭘 원하니? ')
 url = f"https://www.google.com/search?q={query}"
-
+   
 # HTTP 헤더 설정 (필수)
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
