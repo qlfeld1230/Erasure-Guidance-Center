bd3f5d4dc8b2c1d19b6baa76ba9343fe049a5b80
Dong-Han <donghk77@naver.com>
2024-10-31 14:50:20 +0900

    feat : add Comments

--- myproject/app/views/page_view.py
+++ myproject/app/views/page_view.py
@@ -1,19 +1,33 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib import messages
 from django.views import View
+from ..forms import CustomUserCreationForm
 
+''' 페이지 뷰 개요
+- EnrollView 는 회원 가입에 사용하는 컨트롤러(뷰) 입니다.
+- MainView 는 메인 페이지에 관련된 뷰를 만들겁니다.
+- 
+'''
+
+''' 메인 뷰
+- main.html 랜더링 해줌 / 특별한 기능은 없음 현재
+'''
 class MainView(View):
     def get(self, request):
         return render(request, 'main.html')
-    
+
+''' 회원 가입
+- get 및 form 으로 페이지 가져오고
+- post 로 회원 가입 로직
+- post 성공시 login.html 로 리다이렉트
+'''
 class EnrollView(View):
     def get(self, request):
-        form = UserCreationForm()
+        form = CustomUserCreationForm()
         return render(request, 'enroll.html', {'form': form})
 
     def post(self, request):
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, '회원가입이 완료되었습니다.')
