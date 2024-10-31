ced67795e1be251650271ce7cbc0f871f9908d44
Dong-Han <donghk77@naver.com>
2024-10-31 14:47:46 +0900

    feat : add urls

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -5,10 +5,15 @@ from app.views.crawler_view import *
 from django.contrib.auth.views import LogoutView, LoginView
 
 urlpatterns = [
+    # 메인 페이지
     path('', MainView.as_view()),
+    
+    # 회원 가입 및 로그인 관련 -> logout 은 url 처리하면 안 됨, 나중에 수정할 것
     path('admin/', admin.site.urls), 
-    path('login/', LoginView.as_view(template_name='login.html', success_url='/')),
+    path('login/', LoginView.as_view(template_name='login.html', success_url='/'), name='login'),  # name 추가
     path('logout/', LogoutView.as_view(next_page='/login/')), 
     path('enroll/', EnrollView.as_view()),
+    
+    # 크롤러 페이지
     path('crawler/', integrated_crawler_view),
 ]
