f46339291b460d3f721f0419f0735e36af26d9fb
SeongHo Park <qlfeld1324@gmail.com>
2024-11-20 15:51:13 +0900

    feat : add _pychache_

--- myproject/app/admin.py
+++ myproject/app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'password1', 'password2',
+                    'first_name', 'last_name', 'age', 'address', 'school', 'resident_number')
+
+
+admin.site.register(CustomUser)
