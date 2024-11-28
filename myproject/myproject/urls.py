78b5af38723601d302a225223c10b23aac3a4fef
SeongHo Park <qlfeld1324@gmail.com>
2024-11-28 15:32:45 +0900

    fix urls.py

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from app.views.homepage_view import MainView, EnrollView
+from app.views.crawler_view import integrated_crawler_view
 from django.contrib.auth.views import LogoutView, LoginView
 
 from app.views.youtube_view import *
@@ -22,29 +23,32 @@ urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('main/', MainView.as_view(), name='main'),
     path('admin/', admin.site.urls),
-    path('login/', LoginView.as_view(template_name='login.html', success_url='/'), name = 'login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name = 'logout'),
+    path('login/', LoginView.as_view(template_name='login.html',
+         success_url='/'), name='login'),
+    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
     path('enroll/', EnrollView.as_view(), name='enroll'),
     path('crawler/', integrated_crawler_view),
-    path('consent/', ConsentView.as_view(), name='consent'),  # 개인정보 수집 및 이용 동의 페이지 URL
-    
-    
+    path('consent/', ConsentView.as_view(),
+         name='consent'),  # 개인정보 수집 및 이용 동의 페이지 URL
+
+
     # path('', MainView.as_view(), name = 'main'),
 
-     # Django Admin 페이지 URL
+    # Django Admin 페이지 URL
     path('admin/', admin.site.urls),
-    
+
     # 회원 가입 및 로그인 관련 및 홈 크롤러
-    path('homepage/login/', LoginView.as_view(template_name='homepage_login.html'), name='homepage_login'), 
-    path('homepage/logout/', CustomLogoutView.as_view(), name = 'homepage_logout'), 
-    path('homepage/enroll/', EnrollView.as_view(), name = 'homepage_enroll'), 
+    path('homepage/login/', LoginView.as_view(template_name='homepage_login.html'),
+         name='homepage_login'),
+    path('homepage/logout/', CustomLogoutView.as_view(), name='homepage_logout'),
+    path('homepage/enroll/', EnrollView.as_view(), name='homepage_enroll'),
     path('homepage/crawler/', integrated_crawler_view),
 
     # # 회원 가입 및 로그인 관련 및 홈 크롤러
-    # path('homepage/login/', LoginView.as_view(template_name='homepage_login.html', success_url='/'), name='homepage_login'), 
-    # path('homepage/logout/', LogoutView.as_view(next_page='homepage_login/')), 
-    # path('homepage/enroll/', EnrollView.as_view()), 
-    
+    # path('homepage/login/', LoginView.as_view(template_name='homepage_login.html', success_url='/'), name='homepage_login'),
+    # path('homepage/logout/', LogoutView.as_view(next_page='homepage_login/')),
+    # path('homepage/enroll/', EnrollView.as_view()),
+
     # 페이스북 관련 API 및 콜백
     path('main/facebook/login/', Facebook.login, name='facebook_login'),
     path('main/facebook/callback/', Facebook.callback, name='facebook_callback'),
@@ -62,13 +66,14 @@ urlpatterns = [
 
     # 다음
     path('daum/', daum.daum_search, name='daum_search'),
-    
+
     # 트위터
     # 인스타그램
-    
+
     # 유튜브 댓글
-    path('youtube/', redirect_to_google_activity, name='redirect_to_google_activity'),
-    
-    
-    
+    path('youtube/', redirect_to_google_activity,
+         name='redirect_to_google_activity'),
+
+
+
 ]
