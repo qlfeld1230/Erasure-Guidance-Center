15b8411ec1de17b6320bba079230355678400a5f
SeongHo Park <qlfeld1324@gmail.com>
2024-11-28 15:45:46 +0900

    fix hompage_view/main_view

--- myproject/app/views/homepage_view.py
+++ myproject/app/views/homepage_view.py
@@ -11,7 +11,8 @@ import os
 import django
 import sys
 
-sys.path.append("C:/Users/sej68/Documents/GitHub/Erasure-Guidance-Center/myproject")
+sys.path.append(
+    "C:/Users/sej68/Documents/GitHub/Erasure-Guidance-Center/myproject")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
 django.setup()
 
@@ -23,20 +24,25 @@ django.setup()
 ''' 메인 뷰
 - main.html 랜더링 해줌 / 특별한 기능은 없음 현재
 '''
+
+
 class MainView(View):
     def get(self, request):
-        return render(request, 'homepage_main.html')
+        return render(request, 'main.html')
+
 
 ''' 회원 가입
 - get 및 form 으로 페이지 가져오고
 - post 로 회원 가입 로직
 - post 성공시 login.html 로 리다이렉트
 '''
+
+
 class EnrollView(View):
     model = CustomUser
     template_name = 'homepage_enroll.html'
-    success_url = reverse_lazy('homepage_login') # 성공 시 login 페이지로 리다이렉션
-   
+    success_url = reverse_lazy('homepage_login')  # 성공 시 login 페이지로 리다이렉션
+
     def dispatch(self, request, *args, **kwargs):
         if request.method == "POST":
             return self.post(request)
@@ -45,7 +51,7 @@ class EnrollView(View):
     def get(self, request):
         form = CustomUserEnrollForm()
         return render(request, self.template_name, {'form': form})
-        
+
     def post(self, request):
         form = CustomUserEnrollForm(request.POST)
         if form.is_valid():
@@ -71,11 +77,12 @@ class EnrollView(View):
             messages.error(request, "회원가입 중 오류가 발생했습니다.")
             return render(request, self.template_name, {'form': form, 'errors': form.errors})
 
+
 class CustomLogoutView(LogoutView):
     def post(self, request, *args, **kwargs):
         logout(request)  # 로그아웃 처리
         return redirect('main')  # 로그아웃 후 로그인 페이지로 리다이렉트
-     
+
     # GET 요청은 처리하지 않도록 명시적으로 설정
     def get(self, request, *args, **kwargs):
         return redirect('main')  # GET 요청이 오면 로그인 페이지로 리다이렉트
