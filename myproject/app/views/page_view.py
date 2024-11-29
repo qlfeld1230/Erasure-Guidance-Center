96aca02eb22bbcec425b3e31c9436a4bef848b6b
SeongHo Park <qlfeld1324@gmail.com>
2024-11-29 18:27:52 +0900

    add introduce view, html

--- myproject/app/views/page_view.py
+++ myproject/app/views/page_view.py
@@ -39,6 +39,12 @@ class EnrollView(View):
             messages.error(request, '회원가입 중 오류가 발생했습니다. 다시 시도해주세요.')
             return render(request, self.template_name, {'form': form})
 
+
 class ConsentView(View):
     def get(self, request):
         return render(request, 'consent.html')
+
+
+class IntroduceView(View):
+    def post(self, request):
+        return render(request, 'introduce.html')
